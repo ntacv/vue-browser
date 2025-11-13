8480a74ac321b0d37ee292c4927d0cc3cc97ca17
ntacv <nta.ckn@gmail.com>
2025-11-13 15:26:17 +0100

    feat add list page and tab components

---
+++ src/components/list/TabCard.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+
+defineProps<{
+  title: string;
+  url: string;
+  img: string;
+  loaded: boolean;
+}>();
+
+const closeTab = () => {
+  console.log('Close tab clicked');
+};
+
+
+</script>
+<template>
+  <a :href="url" target="_blank">
+    <div id="background">
+      <div id="tab-image">
+        <img :src="img" alt="Tab Image" v-if="loaded" />
+      </div>
+      <div id="tab-info">
+        <h3>{{ title }}</h3>
+        <p>{{ url }}</p>
+      </div>
+      <div id="closeTab" @click="closeTab">x</div>
+  </div>
+</a>
+  </template>
+
+<style scoped>
+
+div#background {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: var(--padding-primary);
+  border-radius: var(--radius-primary);
+  background-color: var(--color-background);
+}
+div#background:hover{
+  border: 1px solid #ccc;
+
+}
+
+a {
+  display: block;
+  padding: 0;
+  margin:0;
+}
+
+#tab-image img {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 5px;
+  color: var(--color-subtext);
+}
+
+#tab-info p {
+  color: var(--color-subtext);
+  font-size: x-small;
+}
+
+#tab-info {
+  flex-grow: 1;
+  margin-left: 15px;
+}
+
+div#closeTab {
+  cursor: pointer;
+  text-align: center;
+  width: 35px;
+  color: var(--danger-primary);
+  padding: var(--padding-secondary);
+  border-radius: var(--radius-secondary);
+  font-weight: bold;
+}
+div#closeTab:hover {
+  background-color: var(--color-background-mute);
+}
+
+
+
+</style>
---
+++ src/views/ListView.vue
@@ -0,0 +1,43 @@
+
+<script setup lang="ts">
+import TabCard from '@/components/list/TabCard.vue';
+import { ref } from 'vue';
+
+const items = ref([
+  {title:"eee", url:"https://google.com",img:"image.png", loaded:true},
+  {title:"rrr", url:"https://bing.com",img:"image2.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+  {title:"ttt", url:"https://duckduckgo.com",img:"image3.png", loaded:true},
+]);
+
+</script>
+
+<template>
+<main>
+<span v-for="(item, index) in items" :key="index">
+  <TabCard :title="item.title" :url="item.url" :img="item.img" :loaded="item.loaded" />
+</span>
+</main>
+
+</template>
